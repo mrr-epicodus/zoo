13a4885550f72a19ab04a87510f507a4d047ae7b
Ryan G Ferris <ryanglenferris@gmail.com>
2018-02-24 16:29:25 -0800

    Edit Animal
    Create the component file for editing an animal's profile on the Zoo's
    program.

---
+++ app/components/edit-animal.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+	selector: 'edit-animal',
+	template: `
+
+  `
+})
+
+export class EditAnimalComponent {
+
+}

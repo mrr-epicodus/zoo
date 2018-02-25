493e7516bdb07d6f3a8514fe2248a44b4840592e
Ryan G Ferris <ryanglenferris@gmail.com>
2018-02-25 14:44:07 -0800

    Animal List Component
    Update the AnimalList_Component Class, and import the Animal Class.

--- app/components/animal-list.component.ts
+++ app/components/animal-list.component.ts
@@ -4,9 +4,17 @@ import { Animal } from './../models/animal.model';
 @Component({
 	selector: 'animal-list',
 	template: `
+
   `
 })
 
-export class AnimalListComponent {
+export class AnimalList_Component {
+	@Input() animals_child: Animal[];
+	@Output() clickSender = new EventEmitter();
 
+	animals: Animal[] = [
+		new Animal("Arctic Fox", "Moon", 2, "Carnivore", "Northern Trail", 5, "Female", "Cool shade", "Loud noises"),
+		new Animal("Ocelot", "Prince", 4, "Carnivore", "Tropical Rain Forest Building", 6, "Male", "Laying in the sunshine", "Toys that are not rope-based"),
+		new Animal("Northwest Black Tailed Deer", "Tinkerbell", 8, "Herbivore", "Northern Trail", 2, "Delicate roots and leaves", "Loud Noises", "Female", )
+	];
 }

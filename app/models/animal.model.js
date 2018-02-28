f07692c98de734faa0a5072e23612f0e25f4464b
Ryan G Ferris <ryanglenferris@gmail.com>
2018-02-27 18:40:09 -0800

    Git ignore
    Update the .gitignore file.

---
+++ app/models/animal.model.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Animal = (function () {
+    function Animal(species, name, age, diet, location, caretakers, sex, likes, dislikes) {
+        this.species = species;
+        this.name = name;
+        this.age = age;
+        this.diet = diet;
+        this.location = location;
+        this.caretakers = caretakers;
+        this.sex = sex;
+        this.likes = likes;
+        this.dislikes = dislikes;
+    }
+    return Animal;
+}());
+exports.Animal = Animal;
+// species: string, name: string, age: number, diet: string, location: string, caretakers: number, sex: string, likes: string, dislikes: string
+// species, name, age, diet, location, caretakers, sex, likes, dislikes
+//# sourceMappingURL=animal.model.js.map

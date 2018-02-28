8e8784ac832223f333f08838a9b5f4242e8c5bf4
Ryan G Ferris <ryanglenferris@gmail.com>
2018-02-27 20:50:50 -0800

    Components
    Update the component files.

--- app/components/animal-list.component.ts
+++ app/components/animal-list.component.ts
@@ -4,17 +4,43 @@ import { Animal } from './../models/animal.model';
 @Component({
 	selector: 'animal-list',
 	template: `
-
+  <div class="form-group">
+  <h3>Filter Animals By Age: </h3>
+  <select (change)="onChange($event.target.value)">
+    <option value="allAnimals" selected="selected">All Animals</option>
+    <option value="juvenile">Juvenile Animals</option>
+    <option value="Mature">Mature Animals</option>
+  </select>
+  </div>
+    <div *ngFor="let currentAnimal of childAnimalList | age:filterByAge">
+			<div class="jumbotron">
+				<h2>{{currentAnimal.name}}</h2>
+        <p>Species: {{currentAnimal.species}}</p>
+        <p>Age: {{currentAnimal.age}}</p>
+        <p>Diet: {{currentAnimal.diet}}</p>
+        <p>Location: {{currentAnimal.location}}</p>
+        <p>Caretakers: {{currentAnimal.caretakers}}</p>
+        <p>Sex: {{currentAnimal.sex}}</p>
+        <p>Likes: {{currentAnimal.likes}}</p>
+        <p>Dislikes: {{currentAnimal.dislikes}}</p>
+        <p><button class="btn btn-primary" (click)="editButtonHasBeenClicked(currentAnimal)">Edit!</button></p>
+			</div>
+		</div>
   `
 })
 
 export class AnimalListComponent {
-	@Input() childAnimals: Animal[];
-	@Output() clickSender = new EventEmitter();
+	@Input() childAnimalList: Animal[];
+	@Output() editClickSender = new EventEmitter();
 
-	animals: Animal[] = [
-		new Animal("Arctic Fox", "Moon", 2, "Carnivore", "Northern Trail", 5, "Female", "Cool shade", "Loud noises"),
-		new Animal("Ocelot", "Prince", 4, "Carnivore", "Tropical Rain Forest Building", 6, "Male", "Laying in the sunshine", "Toys that are not rope-based"),
-		new Animal("Northwest Black Tailed Deer", "Tinkerbell", 8, "Herbivore", "Northern Trail", 2, "Delicate roots and leaves", "Loud Noises", "Female", )
-	];
+	filterByAge: string = "allAnimals";
+
+	editButtonHasBeenClicked(animalToEdit: Animal) {
+		this.editClickSender.emit(animalToEdit);
+	}
+
+	onChange(optionFromMenu) {
+		this.filterByAge = optionFromMenu;
+	}
+
 }
--- app/components/edit-animal.component.ts
+++ app/components/edit-animal.component.ts
@@ -4,10 +4,88 @@ import { Animal } from './../models/animal.model';
 @Component({
 	selector: 'edit-animal',
 	template: `
+	<div class="container">
 
+		<div *ngIf="childSelectedAnimal">
+			<h3>{{childSelectedAnimal.name}}</h3>
+			<p>Species: {{childSelectedAnimal.species}}</p>
+			<hr>
+			<h3>Edit Animal</h3>
+
+			<div class="form-group row">
+				<label class="col-2 col-form-label" for="example-text-input">Name:</label>
+				<div class="col-10">
+					<input [(ngModel)]="childSelectedAnimal.name">
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-2 col-form-label" for="example-text-input">Species:</label>
+				<div class="col-10">
+					<input [(ngModel)]="childSelectedAnimal.species">
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-2 col-form-label" for="example-text-input">Age:</label>
+				<div class="col-10">
+					<input [(ngModel)]="childSelectedAnimal.age">
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-2 col-form-label" for="example-text-input">Diet:</label>
+				<div class="col-10">
+					<input [(ngModel)]="childSelectedAnimal.diet">
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-2 col-form-label" for="example-text-input">Location:</label>
+				<div class="col-10">
+					<input [(ngModel)]="childSelectedAnimal.location">
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-2 col-form-label" for="example-text-input">CareTakers:</label>
+				<div class="col-10">
+					<input [(ngModel)]="childSelectedAnimal.careTakers">
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-2 col-form-label" for="example-text-input">Sex:</label>
+				<div class="col-10">
+					<input [(ngModel)]="childSelectedAnimal.sex">
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-2 col-form-label" for="example-text-input">Likes:</label>
+				<div class="col-10">
+					<input [(ngModel)]="childSelectedAnimal.likes">
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-2 col-form-label" for="example-text-input">Dislikes:</label>
+				<div class="col-10">
+					<input [(ngModel)]="childSelectedAnimal.dislikes">
+				</div>
+
+			</div>
+			<button (click)="doneButtonClicked()">Done</button>
+		</div>
+	</div>
   `
 })
 
 export class EditAnimalComponent {
+	@Input() childSelectedAnimal: Animal;
+	@Output() finishedClickedSender = new EventEmitter();
 
+	doneButtonClicked() {
+		this.finishedClickedSender.emit();
+	}
 }

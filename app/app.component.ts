cbc4c40470268d4f406d8d182c78dfcce3068419
Ryan G Ferris <ryanglenferris@gmail.com>
2018-02-27 19:53:11 -0800

    Animal Component
    Update the animal.component.ts file.

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,15 +5,29 @@ import { Animal } from './models/animal.model';
 	selector: 'app-root',
 	template: `
 	    <div class="container">
-			<h1>ANIMALS</h1>
-			<animal-list></animal-list>
+				<h1>ANIMALS</h1>
+				<animal-list [childAnimals]="masterAnimals" (editClickSender)="editAnimal($event)"></animal-list>
+
+				<h2>UPDATE ANIMAL</h2>
+				<edit-animal [childAnimals]="selectedAnimal" (finishedClickedSender)="finishedEditing()"></edit-animal>
+
+				<h2>NEW ANIMAL</h2>
+				<new-animal [animal-list]="masterAnimals" (editClickSender)="editAnimal($event)"></new-animal>
 			</div>
   `
 })
 
-export class App_Component {
-	animals_master: Animal[] = [
-
+export class AppComponent {
+	animals: Animal[] = [
+		new Animal("Arctic Fox", "Moon", 2, "Carnivore", "Northern Trail", 5, "Female", "Cool shade", "Loud noises"),
+		new Animal("Ocelot", "Prince", 4, "Carnivore", "Tropical Rain Forest Building", 6, "Male", "Laying in the sunshine", "Toys that are not rope-based"),
+		new Animal("Northwest Black Tailed Deer", "Tinkerbell", 8, "Herbivore", "Northern Trail", 2, "Delicate roots and leaves", "Loud Noises", "Female", )
 	];
+
+	selectedAnimal = null;
+
+	editAnimal(clickedAnimal) {
+		this.selectedAnimal = clickedAnimal;
+	}
 
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { App_Component } from './app.component';
-import { AnimalList_Component } from './components/animal-list.component';
+import { AppComponent } from './app.component';
+import { AnimalListComponent } from './components/animal-list.component';
+import { NewAnimalComponent } from './components/new-animal.component';
+import { EditAnimalComponent } from './components/edit-animal.component';
 
 @NgModule({
 	imports: [
 		BrowserModule,
 		FormsModule
-	], declarations: [
-		App_Component,
-		AnimalList_Component
+	],
+	declarations: [
+		AppComponent,
+		AnimalListComponent,
+		NewAnimalComponent,
+		EditAnimalComponent
 	], bootstrap: [
-		App_Component
+		AppComponent
 	]
 })
 

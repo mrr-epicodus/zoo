0f829509fb7410ec41239b894f345893e1298a62
Ryan G Ferris <ryanglenferris@gmail.com>
2018-02-27 19:53:11 -0800

    Animal Component
    Update the animal.component.ts file.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { App_Component } from './app.component';
-import { AnimalList_Component } from './components/animal-list.component';
+import { NgModule }            from '@angular/core';
+import { BrowserModule }       from '@angular/platform-browser';
+import { FormsModule }         from '@angular/forms';
+import { AppComponent }        from './app.component';
+import { AnimalListComponent } from './components/animal-list.component';
 
 @NgModule({
 	imports: [
 		BrowserModule,
 		FormsModule
 	], declarations: [
-		App_Component,
-		AnimalList_Component
+		AppComponent,
+		AnimalListComponent
 	], bootstrap: [
-		App_Component
+		AppComponent
 	]
 })
 
--- app/components/animal-list.component.js
+++ app/components/animal-list.component.js
@@ -10,25 +10,25 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
-var AnimalList_Component = (function () {
-    function AnimalList_Component() {
+var AnimalListComponent = (function () {
+    function AnimalListComponent() {
         this.clickSender = new core_1.EventEmitter();
     }
-    return AnimalList_Component;
+    return AnimalListComponent;
 }());
 __decorate([
     core_1.Input(),
     __metadata("design:type", Array)
-], AnimalList_Component.prototype, "childAnimals", void 0);
+], AnimalListComponent.prototype, "childAnimals", void 0);
 __decorate([
     core_1.Output(),
     __metadata("design:type", Object)
-], AnimalList_Component.prototype, "clickSender", void 0);
-AnimalList_Component = __decorate([
+], AnimalListComponent.prototype, "clickSender", void 0);
+AnimalListComponent = __decorate([
     core_1.Component({
         selector: 'animal-list',
         template: "\n\t<div class=\"form-group\">\n\t<ul>\n\t  <li *ngFor=\"let currentAnimal of childAnimals\">\n\t    <ul>\n\t\t\t\t<h3>Name: {{currentAnimal.name}}</h3>\n\t      <li>Species: {{currentAnimal.species}}</li>\n\t      <li>Age: {{currentAnimal.age}}</li>\n\t      <li>Diet: {{currentAnimal.diet}}</li>\n\t      <li>Location: {{currentAnimal.location}}</li>\n\t      <li>Caretakers: {{currentAnimal.caretakers}}</li>\n\t      <li>Sex: {{currentAnimal.sex}}</li>\n\t      <li>Likes: {{currentAnimal.likes}}</li>\n\t      <li>Dislikes: {{currentAnimal.dislikes}}</li>\n\t      <li><button class=\"btn btn-info\" (click)=\"editButtonHasBeenClicked(currentAnimal)\">Edit!</button></li>\n\t    </ul>\n\t  </li>\n\t</ul>\n  "
     })
-], AnimalList_Component);
-exports.AnimalList_Component = AnimalList_Component;
+], AnimalListComponent);
+exports.AnimalListComponent = AnimalListComponent;
 //# sourceMappingURL=animal-list.component.js.map
